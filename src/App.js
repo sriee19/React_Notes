3a8ff3270f3602fe855e503e0bad20516edd4583
sriee19 <srisanjanaarunkumar@gmail.com>
2023-04-10 20:52:14 +0530

    components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import greet from './components/greet'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello World !
@@ -17,7 +19,16 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+
+    {/*creating a component*/}
+      <greet/>
+      
+
+
+
+
     </div>
   );
 }
---
+++ src/components/greet.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// function greet(){
+//     return <h1>Hello Sriee</h1>
+// }
+
+const greet = () => <h1>Hello Sriee</h1>
+
+
+export default greet
